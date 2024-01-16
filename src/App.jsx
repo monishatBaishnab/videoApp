9421893d94158fd1f8ffe30defa82d1c3bad64a8
Monishat Baishnab <baishnabmonishat@gmail.com>
2024-01-16 19:39:08 +0600

    Create a Study tabs.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navigation/Navbar"
 import SubNav from "./components/Navigation/SubNav"
+import StudyTabs from "./components/StudyContainer/StudyTabs"
 
 function App() {
 
@@ -7,6 +8,10 @@ function App() {
     <>
       <Navbar />
       <SubNav />
+      <div className="container">
+        <h2 className="text-3xl font-medium text-blue-900">Relations and Functions ( Mathematics )</h2>
+        <StudyTabs />
+      </div>
     </>
   )
 }
---
+++ src/components/StudyContainer/StudyTabs.jsx
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+
+const TabItem = ({ label, active }) => {
+    const hoverClass = 'text-[#696671]  hover:text-blue-900 after:absolute after:bottom-0 after:left-0 after:right-0 after:h-1 after:w-0 hover:after:w-full after:bg-blue-900';
+    const activeClass = 'text-blue-900 after:absolute after:bottom-0 after:left-0 after:right-0 after:h-1 after:bg-blue-900';
+    return (
+        <li className="text-[20px]">
+            <a className={`relative px-4 py-3 ${active ? activeClass : hoverClass}`} href="#">{label}</a>
+        </li>
+    );
+}
+
+TabItem.propTypes = {
+    label: PropTypes.string,
+    active: PropTypes.bool,
+}
+
+
+const StudyTabs = () => {
+    const tabs = [
+        {
+            id: 1,
+            label: 'Study'
+        },
+        {
+            id: 2,
+            label: 'Quize'
+        },
+        {
+            id: 3,
+            label: 'Game'
+        },
+        {
+            id: 4,
+            label: 'Others'
+        }
+    ]
+    return (
+        <div className="py-10">
+            <ul className="flex items-center justify-center gap-8">
+                {
+                    tabs?.map(({ id, label }) => <TabItem key={id} label={label} active={id == 1} />)
+                }
+            </ul>
+        </div>
+    );
+};
+
+export default StudyTabs;
